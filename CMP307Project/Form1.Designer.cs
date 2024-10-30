58ba59e8fb112ec18d2fc28937f6f65107ae7fb8
2201587 <2201587@uad.ac.uk>
2024-10-30 17:14:15 +0000

    Created Login UI

--- CMP307Project/Form1.Designer.cs
+++ CMP307Project/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CMP307Project
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.loginTitleLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.submitLoginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginTitleLabel
+            // 
+            this.loginTitleLabel.AutoSize = true;
+            this.loginTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginTitleLabel.Location = new System.Drawing.Point(159, 41);
+            this.loginTitleLabel.Name = "loginTitleLabel";
+            this.loginTitleLabel.Size = new System.Drawing.Size(96, 37);
+            this.loginTitleLabel.TabIndex = 0;
+            this.loginTitleLabel.Text = "Login";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.usernameLabel.Location = new System.Drawing.Point(163, 106);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(77, 18);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "Username";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.passwordLabel.Location = new System.Drawing.Point(163, 170);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(75, 18);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(155, 127);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameTextBox.TabIndex = 3;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(155, 191);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // submitLoginButton
+            // 
+            this.submitLoginButton.Location = new System.Drawing.Point(166, 265);
+            this.submitLoginButton.Name = "submitLoginButton";
+            this.submitLoginButton.Size = new System.Drawing.Size(75, 23);
+            this.submitLoginButton.TabIndex = 5;
+            this.submitLoginButton.Text = "Login";
+            this.submitLoginButton.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(410, 450);
+            this.Controls.Add(this.submitLoginButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.loginTitleLabel);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label loginTitleLabel;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button submitLoginButton;
     }
 }
 
