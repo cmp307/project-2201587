951a61abdf08d88e11fea05513d8408dff2c0813
2201587 <2201587@uad.ac.uk>
2024-11-10 22:31:57 +0000

    Added Delete Asset button functionality

--- CMP307Project/AssetsForm.Designer.cs
+++ CMP307Project/AssetsForm.Designer.cs
@@ -31,15 +31,6 @@
             this.components = new System.ComponentModel.Container();
             this.assetsLbl = new System.Windows.Forms.Label();
             this.assetsTable = new System.Windows.Forms.DataGridView();
-            this.assIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.systemNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.modelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.manufacturerDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.iPAddressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.purchaseDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.notesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.employeeIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.assetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.mssql2201587DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.mssql2201587DataSet = new CMP307Project.mssql2201587DataSet();
@@ -49,6 +40,15 @@
             this.editBtn = new System.Windows.Forms.Button();
             this.refreshBtn = new System.Windows.Forms.Button();
             this.closeBtn = new System.Windows.Forms.Button();
+            this.AssID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.systemNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.manufacturerDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.iPAddressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.purchaseDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.notesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.employeeIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.assetsTable)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.assetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mssql2201587DataSetBindingSource)).BeginInit();
@@ -72,7 +72,7 @@
             this.assetsTable.AutoGenerateColumns = false;
             this.assetsTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.assetsTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.assIDDataGridViewTextBoxColumn,
+            this.AssID,
             this.systemNameDataGridViewTextBoxColumn,
             this.modelDataGridViewTextBoxColumn,
             this.manufacturerDataGridViewTextBoxColumn,
@@ -85,15 +85,85 @@
             this.assetsTable.Location = new System.Drawing.Point(12, 94);
             this.assetsTable.Name = "assetsTable";
             this.assetsTable.ReadOnly = true;
+            this.assetsTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.assetsTable.Size = new System.Drawing.Size(946, 189);
             this.assetsTable.TabIndex = 1;
             // 
-            // assIDDataGridViewTextBoxColumn
+            // assetBindingSource
             // 
-            this.assIDDataGridViewTextBoxColumn.DataPropertyName = "AssID";
-            this.assIDDataGridViewTextBoxColumn.HeaderText = "AssID";
-            this.assIDDataGridViewTextBoxColumn.Name = "assIDDataGridViewTextBoxColumn";
-            this.assIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.assetBindingSource.DataMember = "Asset";
+            this.assetBindingSource.DataSource = this.mssql2201587DataSetBindingSource;
+            // 
+            // mssql2201587DataSetBindingSource
+            // 
+            this.mssql2201587DataSetBindingSource.DataSource = this.mssql2201587DataSet;
+            this.mssql2201587DataSetBindingSource.Position = 0;
+            // 
+            // mssql2201587DataSet
+            // 
+            this.mssql2201587DataSet.DataSetName = "mssql2201587DataSet";
+            this.mssql2201587DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // assetTableAdapter
+            // 
+            this.assetTableAdapter.ClearBeforeFill = true;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(52, 334);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 2;
+            this.addBtn.Text = "Add Asset";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(241, 334);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteBtn.TabIndex = 3;
+            this.deleteBtn.Text = "Delete Asset";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // editBtn
+            // 
+            this.editBtn.Location = new System.Drawing.Point(146, 334);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(75, 23);
+            this.editBtn.TabIndex = 4;
+            this.editBtn.Text = "Edit Asset";
+            this.editBtn.UseVisualStyleBackColor = true;
+            this.editBtn.Click += new System.EventHandler(this.editBtn_Click);
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.Location = new System.Drawing.Point(775, 334);
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.Size = new System.Drawing.Size(75, 23);
+            this.refreshBtn.TabIndex = 5;
+            this.refreshBtn.Text = "Refresh";
+            this.refreshBtn.UseVisualStyleBackColor = true;
+            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Location = new System.Drawing.Point(868, 334);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(75, 23);
+            this.closeBtn.TabIndex = 6;
+            this.closeBtn.Text = "Close";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // AssID
+            // 
+            this.AssID.DataPropertyName = "AssID";
+            this.AssID.HeaderText = "AssID";
+            this.AssID.Name = "AssID";
+            this.AssID.ReadOnly = true;
             // 
             // systemNameDataGridViewTextBoxColumn
             // 
@@ -151,74 +221,6 @@
             this.employeeIDDataGridViewTextBoxColumn.Name = "employeeIDDataGridViewTextBoxColumn";
             this.employeeIDDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // assetBindingSource
-            // 
-            this.assetBindingSource.DataMember = "Asset";
-            this.assetBindingSource.DataSource = this.mssql2201587DataSetBindingSource;
-            // 
-            // mssql2201587DataSetBindingSource
-            // 
-            this.mssql2201587DataSetBindingSource.DataSource = this.mssql2201587DataSet;
-            this.mssql2201587DataSetBindingSource.Position = 0;
-            // 
-            // mssql2201587DataSet
-            // 
-            this.mssql2201587DataSet.DataSetName = "mssql2201587DataSet";
-            this.mssql2201587DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // assetTableAdapter
-            // 
-            this.assetTableAdapter.ClearBeforeFill = true;
-            // 
-            // addBtn
-            // 
-            this.addBtn.Location = new System.Drawing.Point(52, 334);
-            this.addBtn.Name = "addBtn";
-            this.addBtn.Size = new System.Drawing.Size(75, 23);
-            this.addBtn.TabIndex = 2;
-            this.addBtn.Text = "Add Asset";
-            this.addBtn.UseVisualStyleBackColor = true;
-            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
-            // 
-            // deleteBtn
-            // 
-            this.deleteBtn.Location = new System.Drawing.Point(241, 334);
-            this.deleteBtn.Name = "deleteBtn";
-            this.deleteBtn.Size = new System.Drawing.Size(75, 23);
-            this.deleteBtn.TabIndex = 3;
-            this.deleteBtn.Text = "Delete Asset";
-            this.deleteBtn.UseVisualStyleBackColor = true;
-            // 
-            // editBtn
-            // 
-            this.editBtn.Location = new System.Drawing.Point(146, 334);
-            this.editBtn.Name = "editBtn";
-            this.editBtn.Size = new System.Drawing.Size(75, 23);
-            this.editBtn.TabIndex = 4;
-            this.editBtn.Text = "Edit Asset";
-            this.editBtn.UseVisualStyleBackColor = true;
-            this.editBtn.Click += new System.EventHandler(this.editBtn_Click);
-            // 
-            // refreshBtn
-            // 
-            this.refreshBtn.Location = new System.Drawing.Point(775, 334);
-            this.refreshBtn.Name = "refreshBtn";
-            this.refreshBtn.Size = new System.Drawing.Size(75, 23);
-            this.refreshBtn.TabIndex = 5;
-            this.refreshBtn.Text = "Refresh";
-            this.refreshBtn.UseVisualStyleBackColor = true;
-            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
-            // 
-            // closeBtn
-            // 
-            this.closeBtn.Location = new System.Drawing.Point(868, 334);
-            this.closeBtn.Name = "closeBtn";
-            this.closeBtn.Size = new System.Drawing.Size(75, 23);
-            this.closeBtn.TabIndex = 6;
-            this.closeBtn.Text = "Close";
-            this.closeBtn.UseVisualStyleBackColor = true;
-            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
-            // 
             // AssetsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -251,7 +253,12 @@
         private mssql2201587DataSet mssql2201587DataSet;
         private System.Windows.Forms.BindingSource assetBindingSource;
         private mssql2201587DataSetTableAdapters.AssetTableAdapter assetTableAdapter;
-        private System.Windows.Forms.DataGridViewTextBoxColumn assIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button editBtn;
+        private System.Windows.Forms.Button refreshBtn;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AssID;
         private System.Windows.Forms.DataGridViewTextBoxColumn systemNameDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn modelDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn manufacturerDataGridViewTextBoxColumn;
@@ -260,10 +267,5 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn purchaseDateDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn notesDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn employeeIDDataGridViewTextBoxColumn;
-        private System.Windows.Forms.Button addBtn;
-        private System.Windows.Forms.Button deleteBtn;
-        private System.Windows.Forms.Button editBtn;
-        private System.Windows.Forms.Button refreshBtn;
-        private System.Windows.Forms.Button closeBtn;
     }
 }
--- CMP307Project/AssetsForm.cs
+++ CMP307Project/AssetsForm.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace CMP307Project
@@ -52,6 +55,43 @@ namespace CMP307Project
         private void closeBtn_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (assetsTable.SelectedRows.Count == 1)
+                {
+                    int assID = (int)assetsTable.SelectedRows[0].Cells["AssID"].Value;
+                    if (MessageBox.Show("Are you sure you want to delete selected row? (row number: " + assID + ")", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+
+                        Asset asset = (from f in db.Assets
+                                        where f.AssID == assID
+                                        select f).FirstOrDefault();
+                        if (asset != null)
+                        {
+                            db.Assets.Remove(asset);
+                            db.SaveChanges();
+                            loadTable();
+                        }
+                    }
+                    else
+                    {
+                        // user clicked no
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Can only delete one row at a time. Please select only one row and try again.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // exception handler
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
