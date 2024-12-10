9b03aad03a8b57d29eb0e4b3e1df8e19092b0935
2201587 <2201587@uad.ac.uk>
2024-12-10 21:41:47 +0000

    Removed unnecessary code from Edit Asset Form

--- CMP307Project/EditAsset.cs
+++ CMP307Project/EditAsset.cs
@@ -23,51 +23,6 @@ namespace CMP307Project
             this.asset = asset;
         }
 
-        // get data value according to attribute name given
-        private string getData(string name)
-        {
-            // run query to find attribute with the name given
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT " + name + " FROM Win32_ComputerSystem");
-
-            // loop through searcher to find the attribute
-            foreach (ManagementObject obj in searcher.Get())
-            {
-                // if found and has value, return value as string
-                if (obj[name] != null)
-                {
-                    return obj[name].ToString();
-                }
-            }
-
-            // if searcher isnt empty, dispose
-            if (searcher != null)
-            {
-                searcher.Dispose();
-            }
-            // if attribute or value not found, return "unknown"
-            return "unknown";
-        }
-
-        private void autofillBtn_Click(object sender, EventArgs e)
-        {
-            // autofill field values
-            sysNameTB.Text = Environment.MachineName;
-            modelTB.Text = getData("Model");
-            manuTB.Text = getData("Manufacturer");
-            typeTB.Text = getData("SystemType");
-            // get ip address on network
-            string ipAdd = Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).ToString();
-            // if ip not null autofill value, else fill "unavailable"
-            if (ipAdd != null)
-            {
-                ipTB.Text = ipAdd;
-            }
-            else
-            {
-                ipTB.Text = "unavailable";
-            }
-        }
-
         private void cancelBtn_Click(object sender, EventArgs e)
         {
             // close add page
