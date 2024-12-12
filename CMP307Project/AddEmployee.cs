c7b98c9f13e991396357bab5100c2d64b77e5649
2201587 <2201587@uad.ac.uk>
2024-12-12 19:33:30 +0000

    Added password hashing on employee creation and edit

--- CMP307Project/AddEmployee.cs
+++ CMP307Project/AddEmployee.cs
@@ -34,7 +34,7 @@ namespace CMP307Project
                 newEmp.FirstName = firstNameTB.Text;
                 newEmp.LastName = lastNameTB.Text;
                 newEmp.Email = emailTB.Text;
-                newEmp.Password = passwordTB.Text;
+                newEmp.Password = BCrypt.Net.BCrypt.HashPassword(passwordTB.Text);
                 // department ID on the form is automatically set to 0. if it is 0 when the form is submitted, throw an exception as employees must be assigned to a departmnet. if there is a value other than 0, add that as the department ID
                 if (Decimal.ToInt32(departmentNum.Value) == 0)
                 {
--- CMP307Project/EditEmployee.cs
+++ CMP307Project/EditEmployee.cs
@@ -41,7 +41,7 @@ namespace CMP307Project
                     updateEmployee.FirstName = firstNameTB.Text;
                     updateEmployee.LastName = lastNameTB.Text;
                     updateEmployee.Email = emailTB.Text;
-                    updateEmployee.Password = passwordTB.Text;
+                    updateEmployee.Password = BCrypt.Net.BCrypt.HashPassword(passwordTB.Text);
                     // department ID on the form is automatically set to 0. if it is 0 when the form is submitted, throw an exception as employees must be assigned to a departmnet. if there is a value other than 0, add that as the department ID
                     if (Decimal.ToInt32(departmentNum.Value) == 0)
                     {
