7df45d9755d295dc1657d3b981d2c95c5e5b4f10
2201587 <2201587@uad.ac.uk>
2024-12-13 03:40:12 +0000

    Created Add Software form UI

--- CMP307Project/AddSoftware.Designer.cs
+++ CMP307Project/AddSoftware.Designer.cs
@@ -28,12 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.assetNum = new System.Windows.Forms.NumericUpDown();
+            this.autofillBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.assetIDLbl = new System.Windows.Forms.Label();
+            this.manufacturerLbl = new System.Windows.Forms.Label();
+            this.versionLbl = new System.Windows.Forms.Label();
+            this.osNameLbl = new System.Windows.Forms.Label();
+            this.manuTB = new System.Windows.Forms.TextBox();
+            this.modelTB = new System.Windows.Forms.TextBox();
+            this.sysNameTB = new System.Windows.Forms.TextBox();
+            this.addSoftwareLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.assetNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // assetNum
+            // 
+            this.assetNum.Location = new System.Drawing.Point(218, 211);
+            this.assetNum.Name = "assetNum";
+            this.assetNum.Size = new System.Drawing.Size(120, 20);
+            this.assetNum.TabIndex = 40;
+            // 
+            // autofillBtn
+            // 
+            this.autofillBtn.Location = new System.Drawing.Point(67, 257);
+            this.autofillBtn.Name = "autofillBtn";
+            this.autofillBtn.Size = new System.Drawing.Size(75, 43);
+            this.autofillBtn.TabIndex = 39;
+            this.autofillBtn.Text = "Autofill";
+            this.autofillBtn.UseVisualStyleBackColor = true;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(255, 257);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 43);
+            this.cancelBtn.TabIndex = 38;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(162, 257);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 43);
+            this.addBtn.TabIndex = 37;
+            this.addBtn.Text = "Add Software";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // assetIDLbl
+            // 
+            this.assetIDLbl.AutoSize = true;
+            this.assetIDLbl.Location = new System.Drawing.Point(64, 213);
+            this.assetIDLbl.Name = "assetIDLbl";
+            this.assetIDLbl.Size = new System.Drawing.Size(47, 13);
+            this.assetIDLbl.TabIndex = 36;
+            this.assetIDLbl.Text = "Asset ID";
+            // 
+            // manufacturerLbl
+            // 
+            this.manufacturerLbl.AutoSize = true;
+            this.manufacturerLbl.Location = new System.Drawing.Point(64, 177);
+            this.manufacturerLbl.Name = "manufacturerLbl";
+            this.manufacturerLbl.Size = new System.Drawing.Size(74, 13);
+            this.manufacturerLbl.TabIndex = 29;
+            this.manufacturerLbl.Text = "Manufacturer*";
+            // 
+            // versionLbl
+            // 
+            this.versionLbl.AutoSize = true;
+            this.versionLbl.Location = new System.Drawing.Point(64, 140);
+            this.versionLbl.Name = "versionLbl";
+            this.versionLbl.Size = new System.Drawing.Size(46, 13);
+            this.versionLbl.TabIndex = 28;
+            this.versionLbl.Text = "Version*";
+            // 
+            // osNameLbl
+            // 
+            this.osNameLbl.AutoSize = true;
+            this.osNameLbl.Location = new System.Drawing.Point(64, 103);
+            this.osNameLbl.Name = "osNameLbl";
+            this.osNameLbl.Size = new System.Drawing.Size(125, 13);
+            this.osNameLbl.TabIndex = 27;
+            this.osNameLbl.Text = "Operating System Name*";
+            // 
+            // manuTB
+            // 
+            this.manuTB.Location = new System.Drawing.Point(218, 174);
+            this.manuTB.MaxLength = 255;
+            this.manuTB.Name = "manuTB";
+            this.manuTB.Size = new System.Drawing.Size(132, 20);
+            this.manuTB.TabIndex = 24;
+            // 
+            // modelTB
+            // 
+            this.modelTB.Location = new System.Drawing.Point(218, 137);
+            this.modelTB.MaxLength = 255;
+            this.modelTB.Name = "modelTB";
+            this.modelTB.Size = new System.Drawing.Size(132, 20);
+            this.modelTB.TabIndex = 23;
+            // 
+            // sysNameTB
+            // 
+            this.sysNameTB.Location = new System.Drawing.Point(218, 100);
+            this.sysNameTB.MaxLength = 255;
+            this.sysNameTB.Name = "sysNameTB";
+            this.sysNameTB.Size = new System.Drawing.Size(132, 20);
+            this.sysNameTB.TabIndex = 22;
+            // 
+            // addSoftwareLbl
+            // 
+            this.addSoftwareLbl.AutoSize = true;
+            this.addSoftwareLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.addSoftwareLbl.Location = new System.Drawing.Point(103, 27);
+            this.addSoftwareLbl.Name = "addSoftwareLbl";
+            this.addSoftwareLbl.Size = new System.Drawing.Size(210, 37);
+            this.addSoftwareLbl.TabIndex = 21;
+            this.addSoftwareLbl.Text = "Add Software";
+            // 
+            // AddSoftware
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(405, 337);
+            this.Controls.Add(this.assetNum);
+            this.Controls.Add(this.autofillBtn);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.assetIDLbl);
+            this.Controls.Add(this.manufacturerLbl);
+            this.Controls.Add(this.versionLbl);
+            this.Controls.Add(this.osNameLbl);
+            this.Controls.Add(this.manuTB);
+            this.Controls.Add(this.modelTB);
+            this.Controls.Add(this.sysNameTB);
+            this.Controls.Add(this.addSoftwareLbl);
+            this.Name = "AddSoftware";
             this.Text = "AddSoftware";
+            ((System.ComponentModel.ISupportInitialize)(this.assetNum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown assetNum;
+        private System.Windows.Forms.Button autofillBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Label assetIDLbl;
+        private System.Windows.Forms.Label manufacturerLbl;
+        private System.Windows.Forms.Label versionLbl;
+        private System.Windows.Forms.Label osNameLbl;
+        private System.Windows.Forms.TextBox manuTB;
+        private System.Windows.Forms.TextBox modelTB;
+        private System.Windows.Forms.TextBox sysNameTB;
+        private System.Windows.Forms.Label addSoftwareLbl;
     }
 }
