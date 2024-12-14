eaa4599b98a2657b0a1c47d26f7a9322e54d455e
2201587 <2201587@uad.ac.uk>
2024-12-14 01:03:42 +0000

    Amended Edit Software form to recieved passed software link

--- CMP307Project/EditSoftware.cs
+++ CMP307Project/EditSoftware.cs
@@ -15,10 +15,12 @@ namespace CMP307Project
         // setup global variables and database connection
         mssql2201587Entities db = new mssql2201587Entities();
         private Software software;
-        public EditSoftware(Software software)
+        private Link link;
+        public EditSoftware(Software software, Link link)
         {
             InitializeComponent();
             this.software = software;
+            this.link = link;
         }
 
         private void cancelBtn_Click(object sender, EventArgs e)
