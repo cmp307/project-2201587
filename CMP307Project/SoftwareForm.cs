0299537542fb714ec10316e36a4937d8603f75ba
2201587 <2201587@uad.ac.uk>
2024-12-16 17:43:40 +0000

    Added functionality to add link button on software form

--- CMP307Project/SoftwareForm.cs
+++ CMP307Project/SoftwareForm.cs
@@ -146,5 +146,44 @@ namespace CMP307Project
             VulnerabilityCheck newForm = new VulnerabilityCheck();
             newForm.Show();
         }
+
+        private void addLinkBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // check if only one row has been selected
+                if (softwareTable.SelectedRows.Count == 1)
+                {
+                    // get the ID of the row
+                    int softID = (int)softwareTable.SelectedRows[0].Cells["SoftID"].Value;
+                    Software software = (from f in db.Softwares
+                                         where f.SoftID == softID
+                                         select f).FirstOrDefault();
+                    // if asset found, open edit form
+                    if (software != null)
+                    {
+                        Link link = (from f in db.Links
+                                     where f.SoftID == softID
+                                     select f).FirstOrDefault();
+                        if (link != null)
+                        {
+                            // open edit asset form
+                            AddLink newForm = new AddLink(software);
+                            newForm.Show();
+                        }
+                    }
+                }
+                else
+                {
+                    // handle exception if user selects no rows or more than one row
+                    MessageBox.Show("Can only use one row at a time. Please select only one row and try again.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // exception handler
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
