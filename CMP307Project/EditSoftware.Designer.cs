b3f41f22cb3d41a2f441a3704e182c3c284fd5bb
2201587 <2201587@uad.ac.uk>
2024-12-16 17:33:23 +0000

    Removed link editing from edit software

--- CMP307Project/EditSoftware.Designer.cs
+++ CMP307Project/EditSoftware.Designer.cs
@@ -28,10 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.assetNum = new System.Windows.Forms.NumericUpDown();
             this.cancelBtn = new System.Windows.Forms.Button();
             this.editBtn = new System.Windows.Forms.Button();
-            this.assetIDLbl = new System.Windows.Forms.Label();
             this.manufacturerLbl = new System.Windows.Forms.Label();
             this.versionLbl = new System.Windows.Forms.Label();
             this.osNameLbl = new System.Windows.Forms.Label();
@@ -39,20 +37,11 @@
             this.versionTB = new System.Windows.Forms.TextBox();
             this.osNameTB = new System.Windows.Forms.TextBox();
             this.editSoftwareLbl = new System.Windows.Forms.Label();
-            this.activeCB = new System.Windows.Forms.CheckBox();
-            ((System.ComponentModel.ISupportInitialize)(this.assetNum)).BeginInit();
             this.SuspendLayout();
-            // 
-            // assetNum
-            // 
-            this.assetNum.Location = new System.Drawing.Point(213, 216);
-            this.assetNum.Name = "assetNum";
-            this.assetNum.Size = new System.Drawing.Size(120, 20);
-            this.assetNum.TabIndex = 52;
             // 
             // cancelBtn
             // 
-            this.cancelBtn.Location = new System.Drawing.Point(250, 300);
+            this.cancelBtn.Location = new System.Drawing.Point(230, 240);
             this.cancelBtn.Name = "cancelBtn";
             this.cancelBtn.Size = new System.Drawing.Size(75, 43);
             this.cancelBtn.TabIndex = 50;
@@ -62,22 +51,13 @@
             // 
             // editBtn
             // 
-            this.editBtn.Location = new System.Drawing.Point(157, 300);
+            this.editBtn.Location = new System.Drawing.Point(105, 240);
             this.editBtn.Name = "editBtn";
             this.editBtn.Size = new System.Drawing.Size(75, 43);
             this.editBtn.TabIndex = 49;
             this.editBtn.Text = "Edit Software";
             this.editBtn.UseVisualStyleBackColor = true;
             this.editBtn.Click += new System.EventHandler(this.editBtn_Click);
-            // 
-            // assetIDLbl
-            // 
-            this.assetIDLbl.AutoSize = true;
-            this.assetIDLbl.Location = new System.Drawing.Point(59, 218);
-            this.assetIDLbl.Name = "assetIDLbl";
-            this.assetIDLbl.Size = new System.Drawing.Size(47, 13);
-            this.assetIDLbl.TabIndex = 48;
-            this.assetIDLbl.Text = "Asset ID";
             // 
             // manufacturerLbl
             // 
@@ -140,27 +120,13 @@
             this.editSoftwareLbl.TabIndex = 41;
             this.editSoftwareLbl.Text = "Edit Software";
             // 
-            // activeCB
-            // 
-            this.activeCB.AutoSize = true;
-            this.activeCB.Location = new System.Drawing.Point(135, 257);
-            this.activeCB.Name = "activeCB";
-            this.activeCB.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.activeCB.Size = new System.Drawing.Size(62, 17);
-            this.activeCB.TabIndex = 53;
-            this.activeCB.Text = "?Active";
-            this.activeCB.UseVisualStyleBackColor = true;
-            // 
             // EditSoftware
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(405, 372);
-            this.Controls.Add(this.activeCB);
-            this.Controls.Add(this.assetNum);
+            this.ClientSize = new System.Drawing.Size(405, 318);
             this.Controls.Add(this.cancelBtn);
             this.Controls.Add(this.editBtn);
-            this.Controls.Add(this.assetIDLbl);
             this.Controls.Add(this.manufacturerLbl);
             this.Controls.Add(this.versionLbl);
             this.Controls.Add(this.osNameLbl);
@@ -171,18 +137,14 @@
             this.Name = "EditSoftware";
             this.Text = "EditSoftware";
             this.Load += new System.EventHandler(this.EditSoftware_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.assetNum)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.NumericUpDown assetNum;
         private System.Windows.Forms.Button cancelBtn;
         private System.Windows.Forms.Button editBtn;
-        private System.Windows.Forms.Label assetIDLbl;
         private System.Windows.Forms.Label manufacturerLbl;
         private System.Windows.Forms.Label versionLbl;
         private System.Windows.Forms.Label osNameLbl;
@@ -190,6 +152,5 @@
         private System.Windows.Forms.TextBox versionTB;
         private System.Windows.Forms.TextBox osNameTB;
         private System.Windows.Forms.Label editSoftwareLbl;
-        private System.Windows.Forms.CheckBox activeCB;
     }
 }
