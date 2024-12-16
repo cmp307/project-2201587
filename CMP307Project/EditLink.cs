f3d1c02c70be75d2780d7cd93d09e35eb8fcd468
2201587 <2201587@uad.ac.uk>
2024-12-16 18:25:17 +0000

    Added cancel button functionality. Added DB connection and link argument

--- CMP307Project/EditLink.cs
+++ CMP307Project/EditLink.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,17 @@ namespace CMP307Project
 {
     public partial class EditLink : Form
     {
-        public EditLink()
+        mssql2201587Entities db = new mssql2201587Entities();
+        private Link link;
+        public EditLink(Link link)
         {
             InitializeComponent();
+            this.link = link;
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
